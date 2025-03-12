f49d235ce3ee00e7b49d36bc1fb9d62d6f6ba1e5
Mohammed-hussain0307 <fairhussain94@gmail.com>
2025-03-12 17:48:17 +0530

    Update UserModel.cs

--- UserRegistrationForm/Models/UserModel.cs
+++ UserRegistrationForm/Models/UserModel.cs
@@ -1,5 +1,6 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
@@ -10,23 +11,47 @@ namespace UserRegistrationForm.Models
 	{
 		[Key]
 		public int Id { get; set; }
+
+		[DisplayName("First Name")]
 		public string FirstName { get; set; }
+
 		[Required]
-		public string LastName { get; set; }
-		public string DataOfBirth { get; set; }		
-		public string Gender { get; set; }
+        [DisplayName("Last Name")]
+        public string LastName { get; set; }
+
+        [StringLength(10)]
+        [DisplayName("Date of Birth")]
+        public string DataOfBirth { get; set; }
+
+        [DisplayName("Gender")]
+        public string Gender { get; set; }
+
 		[Required]
         [StringLength(10)]
+        [DisplayName("Mobile Number")]
         public string MobileNumber { get; set; }
+
 		[Required]
-		public string EmailAddress { get; set; }
-		[StringLength(200)]
+		[EmailAddress]
+        [DisplayName("Email Address")]
+        public string EmailAddress { get; set; }
+
+        [DisplayName("Address")]
+        [StringLength(200)]
 		public string Address { get; set; }
-		public string City { get; set; }
-		public string State { get; set; }
-		[Required]
+
+        [DisplayName("City")]
+        public string City { get; set; }
+
+        [DisplayName("State")]
+        public string State { get; set; }
+
+        [DisplayName("User Name")]
+        [Required]
 		public string UserName { get; set; }
-		[Required]
+
+        [DisplayName("Password")]
+        [Required]
 		public string PassWord { get; set; }
 	}
 }
